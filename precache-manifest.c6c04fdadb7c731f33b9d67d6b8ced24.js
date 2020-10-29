79922a41653f1a6d57c8def7eab8aaac6289c6ba
Walrussuit101 <tjefferson@ycp.edu>
2020-10-29 14:02:11 -0400

    Updates

---
+++ precache-manifest.c6c04fdadb7c731f33b9d67d6b8ced24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2052088a1912045ba0506fe3990a00f8",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "5b1d5f44f2d0dd1058ee",
+    "url": "/recipes/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "573cfbedffc4c6e58c5f",
+    "url": "/recipes/static/css/main.b853ebbc.chunk.css"
+  },
+  {
+    "revision": "5b1d5f44f2d0dd1058ee",
+    "url": "/recipes/static/js/2.b691656f.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/recipes/static/js/2.b691656f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "573cfbedffc4c6e58c5f",
+    "url": "/recipes/static/js/main.4f2c68e5.chunk.js"
+  },
+  {
+    "revision": "35b65e2aa0053393de9a",
+    "url": "/recipes/static/js/runtime-main.c661edb1.js"
+  }
+]);
